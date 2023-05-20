18bca83a3f1149b104256085c65c32f7f5d933a3
LautaroLopezM <lautarolopezmiguel2077@outlook.com>
2023-05-20 18:47:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar() {
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("html");
+        habilidades[2].classList.add("css");
+        habilidades[3].classList.add("python");
+        habilidades[4].classList.add("photoshop");
+        habilidades[5].classList.add("excel");
+        habilidades[6].classList.add("blender");
+        habilidades[7].classList.add("wordpress");
+        habilidades[8].classList.add("comunicacion");
+        habilidades[9].classList.add("trabajo");
+        habilidades[10].classList.add("creatividad");
+        habilidades[11].classList.add("proyect");
+        habilidades[12].classList.add("dedicacion");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function () {
+    efectoHabilidades();
+}
